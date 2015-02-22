3569fbcb695fb244fe21d31825e3bc952f97af67
Christine Moy <christinemoy@gmail.com>
2015-02-21 20:02:43 -0500

    Homework 1

---
+++ Homework 01/Lesson1TakeTwo/Lesson1TakeTwo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lesson1TakeTwo
+//
+//  Created by Christine Moy on 2/21/15.
+//  Copyright (c) 2015 Christine Moy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
